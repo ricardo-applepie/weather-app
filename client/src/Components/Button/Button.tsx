8d8314b640c2309d734dc34611c39c4e82ba1e51
Fullstack web developer <84002031+ricardo-applepie@users.noreply.github.com>
2025-02-13 22:06:38 +0100

    fixed button not functioning properly

--- client/src/Components/Button/Button.tsx
+++ client/src/Components/Button/Button.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
+
 interface ButtonProps {
-  text:string; 
-  buttonAction:Function
+  text: string; 
+  buttonAction: Function;
 }
 
-function Button({ text, buttonAction }:ButtonProps) {
-  // reusable button  component  with props ;
+function Button({ text, buttonAction }: ButtonProps) {
   return (
     <React.Fragment>
       <button onClick={buttonAction}>{text}</button>
     </React.Fragment>
   );
 }
-
 export default Button;
