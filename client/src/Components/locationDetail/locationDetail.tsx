190446eaaea1b858f889894aeb82c2f4c5a4dfea
Fullstack web developer <84002031+ricardo-applepie@users.noreply.github.com>
2025-02-13 22:05:43 +0100

    updated code style

--- client/src/Components/locationDetail/locationDetail.tsx
+++ client/src/Components/locationDetail/locationDetail.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 function LocationDetail() { 
-    return (
-        <div>
-
-        </div>
-    )
+  return (
+    <div> </div>
+  )
 }
-export default LocationDetail ; 
+export default LocationDetail; 
